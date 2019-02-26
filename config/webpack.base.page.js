af081cb502ef0f6c814fcad118ab466792fa9097
Cat Y <fonahuf1@163.com>
2019-02-26 18:50:52 +0800

    调整view目录结构

--- config/webpack.base.page.js
+++ config/webpack.base.page.js
@@ -18,128 +18,128 @@ function webpackPageConfig(HtmlWebpackPlugin) {
 
     new HtmlWebpackPlugin({
       title: '简单工厂模式',
-      filename: '../views/simple_factory.html',
-      template: 'static/views/simple_factory.html',
+      filename: '../views/pattern/simple_factory.html',
+      template: 'static/views/pattern/simple_factory.html',
       chunks: [ 'base', 'simple_factory.main'],
       chunksSortMode: 'manual'
     }),
 
     new HtmlWebpackPlugin({
       title: '抽象工厂模式',
-      filename: '../views/abstract_factory.html',
-      template: 'static/views/abstract_factory.html',
+      filename: '../views/pattern/abstract_factory.html',
+      template: 'static/views/pattern/abstract_factory.html',
       chunks: [ 'base', 'abstract_factory.main'],
       chunksSortMode: 'manual'
     }),
 
     new HtmlWebpackPlugin({
       title: '建造者模式',
-      filename: '../views/builder_factory.html',
-      template: 'static/views/builder_factory.html',
+      filename: '../views/pattern/builder_factory.html',
+      template: 'static/views/pattern/builder_factory.html',
       chunks: [ 'base', 'builder_factory.main'],
       chunksSortMode: 'manual'
     }),
 
     new HtmlWebpackPlugin({
       title: '适配器模式',
-      filename: '../views/adapter_factory.html',
-      template: 'static/views/adapter_factory.html',
+      filename: '../views/pattern/adapter_factory.html',
+      template: 'static/views/pattern/adapter_factory.html',
       chunks: [ 'base', 'adapter_factory.main'],
       chunksSortMode: 'manual'
     }),
 
     new HtmlWebpackPlugin({
       title: '装饰者模式',
-      filename: '../views/decorator_factory.html',
-      template: 'static/views/decorator_factory.html',
+      filename: '../views/pattern/decorator_factory.html',
+      template: 'static/views/pattern/decorator_factory.html',
       chunks: [ 'base', 'decorator_factory.main'],
       chunksSortMode: 'manual'
     }),
 
     new HtmlWebpackPlugin({
       title: '桥接模式',
-      filename: '../views/bridge_factory.html',
-      template: 'static/views/bridge_factory.html',
+      filename: '../views/pattern/bridge_factory.html',
+      template: 'static/views/pattern/bridge_factory.html',
       chunks: [ 'base', 'bridge_factory.main'],
       chunksSortMode: 'manual'
     }),
 
     new HtmlWebpackPlugin({
       title: '组合模式',
-      filename: '../views/group_factory.html',
-      template: 'static/views/group_factory.html',
+      filename: '../views/pattern/group_factory.html',
+      template: 'static/views/pattern/group_factory.html',
       chunks: [ 'base', 'group_factory.main'],
       chunksSortMode: 'manual'
     }),
 
     new HtmlWebpackPlugin({
       title: '享元模式',
-      filename: '../views/share_factory.html',
-      template: 'static/views/share_factory.html',
+      filename: '../views/pattern/share_factory.html',
+      template: 'static/views/pattern/share_factory.html',
       chunks: [ 'base', 'share_factory.main'],
       chunksSortMode: 'manual'
     }),
 
     new HtmlWebpackPlugin({
       title: '模板方法模式',
-      filename: '../views/template_factory.html',
-      template: 'static/views/template_factory.html',
+      filename: '../views/pattern/template_factory.html',
+      template: 'static/views/pattern/template_factory.html',
       chunks: [ 'base', 'template_factory.main'],
       chunksSortMode: 'manual'
     }),
 
     new HtmlWebpackPlugin({
       title: '观察者模式',
-      filename: '../views/observers_factory.html',
-      template: 'static/views/observers_factory.html',
+      filename: '../views/pattern/observers_factory.html',
+      template: 'static/views/pattern/observers_factory.html',
       chunks: [ 'base', 'observers_factory.main'],
       chunksSortMode: 'manual'
     }),
 
     new HtmlWebpackPlugin({
       title: '状态模式',
-      filename: '../views/state_factory.html',
-      template: 'static/views/state_factory.html',
+      filename: '../views/pattern/state_factory.html',
+      template: 'static/views/pattern/state_factory.html',
       chunks: [ 'base', 'state_factory.main'],
       chunksSortMode: 'manual'
     }),
 
     new HtmlWebpackPlugin({
       title: '策略模式',
-      filename: '../views/strategy_factory.html',
-      template: 'static/views/strategy_factory.html',
+      filename: '../views/pattern/strategy_factory.html',
+      template: 'static/views/pattern/strategy_factory.html',
       chunks: [ 'base', 'strategy_factory.main'],
       chunksSortMode: 'manual'
     }),
 
     new HtmlWebpackPlugin({
       title: '责任链模式',
-      filename: '../views/response_chain_factory.html',
-      template: 'static/views/response_chain_factory.html',
+      filename: '../views/pattern/response_chain_factory.html',
+      template: 'static/views/pattern/response_chain_factory.html',
       chunks: [ 'base', 'response_chain_factory.main'],
       chunksSortMode: 'manual'
     }),
 
     new HtmlWebpackPlugin({
       title: '命令模式',
-      filename: '../views/command_factory.html',
-      template: 'static/views/command_factory.html',
+      filename: '../views/pattern/command_factory.html',
+      template: 'static/views/pattern/command_factory.html',
       chunks: [ 'base', 'command_factory.main'],
       chunksSortMode: 'manual'
     }),
 
     new HtmlWebpackPlugin({
       title: '中介模式',
-      filename: '../views/intermediary_factory.html',
-      template: 'static/views/intermediary_factory.html',
+      filename: '../views/pattern/intermediary_factory.html',
+      template: 'static/views/pattern/intermediary_factory.html',
       chunks: [ 'base', 'intermediary_factory.main'],
       chunksSortMode: 'manual'
     }),
 
     new HtmlWebpackPlugin({
       title: '解释器模式',
-      filename: '../views/interpreter_factory.html',
-      template: 'static/views/interpreter_factory.html',
+      filename: '../views/pattern/interpreter_factory.html',
+      template: 'static/views/pattern/interpreter_factory.html',
       chunks: [ 'base', 'interpreter_factory.main'],
       chunksSortMode: 'manual'
     }),
--- src/routes/routes.pattern.controller.ts
+++ src/routes/routes.pattern.controller.ts
@@ -4,7 +4,7 @@ import { Get, Controller, Req, Res, Next } from '@nestjs/common';
 export class RoutesPatternController {
   @Get('/simple_factory')
   simpleFactory(@Res() res) {
-    res.render('./simple_factory.html', {
+    res.render('./pattern/simple_factory.html', {
       title: '简单工厂模式',
       mainMenu: 'simple_factory',
       toggleMenu: 'pattern'
@@ -13,7 +13,7 @@ export class RoutesPatternController {
 
   @Get('/abstract_factory')
   abstractFactory(@Res() res) {
-    res.render('./abstract_factory.html', {
+    res.render('./pattern/abstract_factory.html', {
       title: '抽象工厂模式',
       mainMenu: 'abstract_factory',
       toggleMenu: 'pattern'
@@ -22,7 +22,7 @@ export class RoutesPatternController {
 
   @Get('/builder_factory')
   builderFactory(@Res() res) {
-    res.render('./builder_factory.html', {
+    res.render('./pattern/builder_factory.html', {
       title: '建造者模式',
       mainMenu: 'builder_factory',
       toggleMenu: 'pattern'
@@ -31,7 +31,7 @@ export class RoutesPatternController {
 
   @Get('/adapter_factory')
   adapterFactory(@Res() res) {
-    res.render('./adapter_factory.html', {
+    res.render('./pattern/adapter_factory.html', {
       title: '适配器模式',
       mainMenu: 'adapter_factory',
       toggleMenu: 'pattern'
@@ -40,7 +40,7 @@ export class RoutesPatternController {
 
   @Get('/decorator_factory')
   decoratorFactory(@Res() res) {
-    res.render('./decorator_factory.html', {
+    res.render('./pattern/decorator_factory.html', {
       title: '装饰者模式',
       mainMenu: 'decorator_factory',
       toggleMenu: 'pattern'
@@ -49,7 +49,7 @@ export class RoutesPatternController {
 
   @Get('/bridge_factory')
   bridgeFactory(@Res() res) {
-    res.render('./bridge_factory.html', {
+    res.render('./pattern/bridge_factory.html', {
       title: '桥接模式',
       mainMenu: 'bridge_factory',
       toggleMenu: 'pattern'
@@ -58,7 +58,7 @@ export class RoutesPatternController {
 
   @Get('/group_factory')
   groupFactory(@Res() res) {
-    res.render('./group_factory.html', {
+    res.render('./pattern/group_factory.html', {
       title: '组合模式',
       mainMenu: 'group_factory',
       toggleMenu: 'pattern'
@@ -67,7 +67,7 @@ export class RoutesPatternController {
 
   @Get('/share_factory')
   shareFactory(@Res() res) {
-    res.render('./share_factory.html', {
+    res.render('./pattern/share_factory.html', {
       title: '享元模式',
       mainMenu: 'share_factory',
       toggleMenu: 'pattern'
@@ -76,7 +76,7 @@ export class RoutesPatternController {
 
   @Get('/template_factory')
   templateFactory(@Res() res) {
-    res.render('./template_factory.html', {
+    res.render('./pattern/template_factory.html', {
       title: '模板方法模式',
       mainMenu: 'template_factory',
       toggleMenu: 'pattern'
@@ -85,7 +85,7 @@ export class RoutesPatternController {
 
   @Get('/observers_factory')
   observersFactory(@Res() res) {
-    res.render('./observers_factory.html', {
+    res.render('./pattern/observers_factory.html', {
       title: '观察者模式',
       mainMenu: 'observers_factory',
       toggleMenu: 'pattern'
@@ -94,7 +94,7 @@ export class RoutesPatternController {
 
   @Get('/state_factory')
   stateFactory(@Res() res) {
-    res.render('./state_factory.html', {
+    res.render('./pattern/state_factory.html', {
       title: '状态模式',
       mainMenu: 'state_factory',
       toggleMenu: 'pattern'
@@ -103,7 +103,7 @@ export class RoutesPatternController {
 
   @Get('/strategy_factory')
   strategyFactory(@Res() res) {
-    res.render('./strategy_factory.html', {
+    res.render('./pattern/strategy_factory.html', {
       title: '策略模式',
       mainMenu: 'strategy_factory',
       toggleMenu: 'pattern'
@@ -112,7 +112,7 @@ export class RoutesPatternController {
 
   @Get('/response_chain_factory')
   responseChainFactory(@Res() res) {
-    res.render('./response_chain_factory.html', {
+    res.render('./pattern/response_chain_factory.html', {
       title: '责任链模式',
       mainMenu: 'response_chain_factory',
       toggleMenu: 'pattern'
@@ -121,7 +121,7 @@ export class RoutesPatternController {
 
   @Get('/command_factory')
   commandFactory(@Res() res) {
-    res.render('./command_factory.html', {
+    res.render('./pattern/command_factory.html', {
       title: '命令模式',
       mainMenu: 'command_factory',
       toggleMenu: 'pattern'
@@ -130,7 +130,7 @@ export class RoutesPatternController {
 
   @Get('/intermediary_factory')
   intermediaryFactory(@Res() res) {
-    res.render('./intermediary_factory.html', {
+    res.render('./pattern/intermediary_factory.html', {
       title: '中介模式',
       mainMenu: 'intermediary_factory',
       toggleMenu: 'pattern'
@@ -139,7 +139,7 @@ export class RoutesPatternController {
 
   @Get('/interpreter_factory')
   interpreterFactory(@Res() res) {
-    res.render('./interpreter_factory.html', {
+    res.render('./pattern/interpreter_factory.html', {
       title: '解释器模式',
       mainMenu: 'interpreter_factory',
       toggleMenu: 'pattern'
